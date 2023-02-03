6f40152d88558c5e2dc625c607fb8677aac74e6d
Artur <110197493+makishima44@users.noreply.github.com>
2023-02-03 09:14:49 +0300

    Categorize New Member

---
+++ 207.js
@@ -0,0 +1,11 @@
+function openOrSenior(data) {
+  let newArray = [];
+  for (let i = 0; i < data.length; i++) {
+    if (data[i][0] >= 55 && data[i][1] > 7) {
+      newArray.push("Senior");
+    } else {
+      newArray.push("Open");
+    }
+  }
+  return newArray;
+}
