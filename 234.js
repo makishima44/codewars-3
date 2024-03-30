69f1e0642d68c21d231d4ec5573452e8d5d5b046
Artur <metallist15431543@gmail.com>
2024-03-30 10:02:04 +0300

    Determine offspring sex based on genes XX and XY chromosomes

---
+++ 234.js
@@ -0,0 +1,6 @@
+function chromosomeCheck(sperm) {
+  if (sperm == "XY") {
+    return "Congratulations! You're going to have a son.";
+  }
+  return "Congratulations! You're going to have a daughter.";
+}
