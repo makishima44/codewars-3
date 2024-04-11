01f7b8dcf6c8d38f9b9f5366b359c8d050aa4893
Artur <metallist15431543@gmail.com>
2024-04-11 21:30:25 +0300

    L1: Bartender, drinks!

---
+++ 238.js
@@ -0,0 +1,21 @@
+function getDrinkByProfession(param) {
+  let inputLower = param.toLowerCase();
+  switch (inputLower) {
+    case "jabroni":
+      return "Patron Tequila";
+    case "school counselor":
+      return "Anything with Alcohol";
+    case "programmer":
+      return "Hipster Craft Beer";
+    case "bike gang member":
+      return "Moonshine";
+    case "politician":
+      return "Your tax dollars";
+    case "rapper":
+      return "Cristal";
+    default:
+      return "Beer";
+  }
+}
+
+console.log(getDrinkByProfession("jabrOni"));
