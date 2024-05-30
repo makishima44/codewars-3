0d896ecd31e80a812255e77562a57a1df6cf3c00
Artur <metallist15431543@gmail.com>
2024-05-30 19:27:10 +0300

    Implement Array.prototype.filter()

---
+++ 269.js
@@ -0,0 +1,10 @@
+Array.prototype.filter = function (func) {
+  let filtered = [];
+
+  for (let i = 0; i < this.length; i++) {
+    if (func(this[i], i, this)) {
+      filtered.push(this[i]);
+    }
+  }
+  return filtered;
+};
