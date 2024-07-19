6c73b36e2adf97e62a9979b68de2e0da423bc2f5
Artur <metallist15431543@gmail.com>
2024-07-19 13:24:00 +0300

    Is the date today

---
+++ 287.js
@@ -0,0 +1,11 @@
+function isToday(date) {
+  const today = new Date();
+
+  return (
+    date.getFullYear() === today.getFullYear() &&
+    date.getMonth() === today.getMonth() &&
+    date.getDate() === today.getDate()
+  );
+}
+
+isToday();
