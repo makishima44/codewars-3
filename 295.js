68068ab4fcd18109de4d3cc7368c4a2d928baa20
Artur <metallist15431543@gmail.com>
2024-08-05 19:00:17 +0300

    They say that only the name is long enough to attract attention. They also said that only a simple Kata will have someone to solve it. This is a sadly story #1: Are they opposite?

---
+++ 295.js
@@ -0,0 +1,20 @@
+function isOpposite(s1, s2) {
+  if (s1.length === 0 || s2.length === 0) {
+    return false;
+  }
+  if (s1.length !== s2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < s1.length; i++) {
+    if (s1[i] === s2[i]) {
+      return false;
+    }
+    if (s1[i].toLowerCase() !== s2[i].toLowerCase()) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(isOpposite("AB", "Ab"));
